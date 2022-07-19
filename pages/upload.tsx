44f907bb1c1615c9899cf537af3f756ce9d40aba
Faysal Bsata <faysalbsata@hotmail.com>
2022-07-19 12:44:13 +0400

    home page mods

--- pages/upload.tsx
+++ pages/upload.tsx
@@ -7,6 +7,7 @@ import useAuthStore from "../store/authStore";
 import { client } from "../utils/client";
 import { SanityAssetDocument } from "@sanity/client";
 import { topics } from "../utils/constants";
+import { BASE_URL } from "../utils";
 const Upload = () => {
   const router = useRouter();
   const [isLoading, setIsLoading] = useState(false);
@@ -57,7 +58,7 @@ const Upload = () => {
         topic: category,
       };
       console.log(document);
-      await axios.post("http://localhost:3000/api/post", document);
+      await axios.post(`${BASE_URL}/api/post`, document);
       router.push("/");
     }
   };
